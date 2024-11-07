e7991082f573f987290f490b5bcfe3f945d1d6fa
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-11-07 20:21:04 +0330

    Create User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
