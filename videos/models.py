4694de18401051583f93121fe837da14c46272c2
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-11-07 21:10:42 +0330

    Create Comment model

--- videos/models.py
+++ videos/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
