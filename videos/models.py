e09ec744d23bebdc96e8e961383d24ae84914b52
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-11-07 21:11:16 +0330

    Create Playlist model

--- videos/models.py
+++ videos/models.py
@@ -10,3 +10,8 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
