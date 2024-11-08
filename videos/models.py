e2676cffacdc5fbed0b22e5b6898e7765eb6dcf7
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-11-08 20:11:41 +0330

    Add more fields to Video model

--- videos/models.py
+++ videos/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from django.conf import settings
-
+from django.utils.translation import gettext_lazy as _
 
 class Video(models.Model):
+    class Privacy(models.TextChoices):
+        PUBLIC = 'public', _('Public')
+        PRIVATE = 'private', _('Private')
+        ULISTED = 'unlisted', _('Unlisted')
+
+    class Status(models.TextChoices):
+        PROCESSING = 'processing', _('Processing')
+        PUBLISHED = 'published', _('Published')
+        FAILED = 'failed', _('Failed')
+
     title = models.CharField(max_length=255)
     description = models.TextField()
+    file_path = models.URLField()
+    thumbnail = models.ImageField()
+    duration = models.DurationField()
+    views = models.IntegerField()
+    uploaded_at = models.DateTimeField()
+    privacy = models.CharField(max_length=8, choices=Privacy)
+    status = models.CharField(max_length=10, choices=Status)
 
 
 class Comment(models.Model):
